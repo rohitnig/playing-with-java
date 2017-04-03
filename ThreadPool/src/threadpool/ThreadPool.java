a2a2d6dc640fa7acf16345fbacb8b18de452e8b5
rpnigam <rpnigam@NigamsTP>
2017-04-02 23:31:04 -0400

    Playing around

---
+++ ThreadPool/src/threadpool/ThreadPool.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threadpool;
+
+import java.util.concurrent.ExecutorService;  
+import java.util.concurrent.Executors;  
+
+/**
+ *
+ * @author rpnigam
+ */
+public class ThreadPool {
+
+    /**
+     * @param args the command line arguments
+     */
+     public static void main(String[] args) {  
+        ExecutorService executor = Executors.newFixedThreadPool(5);//creating a pool of 5 threads  
+        for (int i = 0; i < 10; i++) {  
+            Runnable worker = new WorkerThread("" + i);  
+            executor.execute(worker);//calling execute method of ExecutorService  
+          }  
+        executor.shutdown();  
+        while (!executor.isTerminated()) {   }  
+  
+        System.out.println("Finished all threads");  
+    }      
+}

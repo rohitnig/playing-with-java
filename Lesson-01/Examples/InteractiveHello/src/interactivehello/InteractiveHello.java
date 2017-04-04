c9deb503251e62bc69979df07a623ef08b7dd1c9
rpnigam <rpnigam@NigamsTP>
2017-04-04 00:17:36 -0400

    More ...

--- Lesson-01/Examples/InteractiveHello/src/interactivehello/InteractiveHello.java
+++ Lesson-01/Examples/InteractiveHello/src/interactivehello/InteractiveHello.java
@@ -14,8 +14,22 @@ public class InteractiveHello {
 
     private static void playon() {
         int i[] = {1};
+        int large_array[] = new int[255];
+        int max_int=255;
         change_i(i);
+        
+        large_array[254]=8; 
+        
+        large_array[253]=10;
         System.out.println("i:"+i[0]);
+        System.err.println("max_int is "+max_int);
+        System.err.println("array length is "+large_array.length+" and the last one is " + 
+                large_array[large_array.length-1]);
+
+        if( "Welcome".trim() == "Welcome".trim() )
+        System.out.println("Equal");
+        else
+        System.out.println("Not Equal");
     }
     
     public static void change_i(int i[]) 
@@ -31,9 +45,9 @@ public class InteractiveHello {
     public static void main(String[] args) {
         // make new object of class Scanner, based on System.in and call it input
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter your name ==> ");                
-        String yourName = input.nextLine();
-        System.out.printf("Hello, %s!!!!!\n", yourName); // say hello
+        //System.out.print("Enter your name ==> ");                
+        //String yourName = input.nextLine();
+        System.out.printf("Hello, %s!!!!!\n", "rohit"); // say hello
         System.err.println("" + (14^23));
         System.err.println(""+ ("Welcome".trim()=="Welcome".trim()));
         playon();
